baaf30c9ae672e81705bba6d2ab50937e2b1349d
Jake <jakeroggenbuck2@gmail.com>
2022-11-05 13:55:30 -0700

    Add url and have requirements install

--- setup.py
+++ setup.py
@@ -3,6 +3,9 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
 setuptools.setup(
     name="magic-identify",
     version="0.1.2",
@@ -11,7 +14,7 @@ setuptools.setup(
     description="A python module and command to try really hard to get a mime-type for a file",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/gawseed/pyfsdb",
+    url="https://github.com/hardaker/magic-identify",
     packages=setuptools.find_packages(),
     entry_points={
         'console_scripts': [
@@ -24,6 +27,7 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     python_requires = '>=3.6',
+    install_requires=required,
     test_suite='nose.collector',
     tests_require=['nose'],
 )
