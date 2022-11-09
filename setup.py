aaa612290fdfb454047807836bd0dd48a1dee16d
Wes Hardaker <opensource@hardakers.net>
2022-11-09 05:39:30 -0800

    fix requirements specifications

--- setup.py
+++ setup.py
@@ -3,9 +3,6 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-with open("requirements.txt", "r") as f:
-    required = f.read().splitlines()
-
 setuptools.setup(
     name="magic-identify",
     version="0.2.1",
@@ -17,17 +14,17 @@ setuptools.setup(
     url="https://github.com/hardaker/magic-identify",
     packages=setuptools.find_packages(),
     entry_points={
-        'console_scripts': [
+        "console_scripts": [
             # migrating to pdb prefixes
-            'magic-identify = magicidentify.tools.magicidentifycli:main',
+            "magic-identify = magicidentify.tools.magicidentifycli:main",
         ]
     },
     classifiers=[
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
     ],
-    python_requires = '>=3.6',
-    install_requires=required,
-    test_suite='nose.collector',
-    tests_require=['nose'],
+    python_requires=">=3.6",
+    install_requires=["python-magic >= 0.4.24", "identify >= 2.2.13"],
+    test_suite="nose.collector",
+    tests_require=["nose"],
 )
